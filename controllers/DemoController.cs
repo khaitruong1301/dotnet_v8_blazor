0631b14e1eeb161c94e61b7262d58ad4da9ebf9f
khai <khaitruong1301@gmail.com>
2025-03-03 14:08:30 +0700

    dp

---
+++ controllers/DemoController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using web_api_base.Models;
+
+namespace web_api_base.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        public DemoController()
+        {
+        }
+
+        [HttpGet("getall")]
+        public async Task<ActionResult> GetAll()
+        {
+            
+            return Ok("ok");
+        }
+
+       
+
+    }
+}
